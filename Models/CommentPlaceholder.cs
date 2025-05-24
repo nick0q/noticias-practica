3b5ffe6a0bdd6fd82f45490b8cfec3500e24039f
nick0q <nickquezada5@gmail.com>
2025-05-24 00:28:35 -0500

    integrando mvc del portal

---
+++ Models/CommentPlaceholder.cs
@@ -0,0 +1,11 @@
+namespace noticias.Models
+{
+    public class CommentPlaceholder
+    {
+        public int PostId { get; set; }
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Body { get; set; }
+    }
+}
---
+++ Models/UserPlaceholder.cs
@@ -0,0 +1,11 @@
+namespace noticias.Models
+{
+    public class UserPlaceholder
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        // Añade otras propiedades si las necesitas
+    }
+}
